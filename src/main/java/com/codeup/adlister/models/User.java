279c9232616b2b18c3757a780a8c7733899c61a3
Bianca Reusch <reusch.bianca@gmail.com>
2020-11-17 15:34:29 -0600

    feat: added new user columns to DAO and User model, as well as registration page and methods

--- src/main/java/com/codeup/adlister/models/User.java
+++ src/main/java/com/codeup/adlister/models/User.java
@@ -5,22 +5,32 @@ import com.codeup.adlister.util.Password;
 public class User {
     private long id;
     private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
+    private Boolean isAdmin;
+
 
     public User() {}
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password,String first_name,String last_name) {
         this.username = username;
         this.email = email;
         setPassword(password);
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.isAdmin = false;
     }
 
-    public User(long id, String username, String email, String password) {
+    public User(long id, String username, String email, String password, String first_name, String last_name) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.password = password;
+        this.firstName = first_name;
+        this.lastName = last_name;
+        this.isAdmin = false;
     }
 
     public long getId() {
@@ -53,5 +63,23 @@ public class User {
 
     public void setPassword(String password) {
         this.password = Password.hash(password);
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public void setFirstName(String first_name){
+        this.firstName = first_name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String last_name){
+        this.lastName = last_name;
+    }
+    public Boolean isAdmin(){
+        return this.isAdmin;
+    }
+    public void setAsAdmin(Boolean isAdmin){
+        this.isAdmin = isAdmin;
     }
 }
